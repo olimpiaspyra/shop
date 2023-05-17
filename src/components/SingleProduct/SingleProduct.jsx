664ef09283fc8fa05dfe7c88a26fa8e34802dcb0
Olimpia Spyra <olimpia.spyra@gmail.com>
2023-05-17 18:09:20 +0200

    Add func add to cart and remove from cart

--- src/components/SingleProduct/SingleProduct.jsx
+++ src/components/SingleProduct/SingleProduct.jsx
@@ -3,8 +3,16 @@ import {PropTypes} from 'prop-types';
 import {Card, Button} from 'react-bootstrap';
 
 import Rating from '../Rating/Rating';
+import {CartState} from '../../context/Context';
 
 const SingleProduct = ({prod}) => {
+  const {
+    state: {cart},
+    dispatch,
+  } = CartState();
+
+  console.log(cart);
+
   return (
     <div className='products' style={{width: '30%', margin: 10}}>
       <Card>
@@ -17,10 +25,31 @@ const SingleProduct = ({prod}) => {
             {prod.fastDelivery ? <div>Fast Delivery</div> : <div>4 days delivery</div>}
             <Rating className='py-5' rating={prod.ratings} />
           </Card.Subtitle>
-          <Button variant='danger'>Remove from cart</Button>
-          <Button disabled={!prod.inStock}>
-            {!prod.inStock ? 'Out of stock' : 'Add to cart'}
-          </Button>
+          {cart.some((p) => p.id === prod.id) ? (
+            <Button
+              onClick={() => {
+                dispatch({
+                  type: 'REMOVE_FROM_CART',
+                  payload: prod,
+                });
+              }}
+              variant='danger'
+            >
+              Remove from cart
+            </Button>
+          ) : (
+            <Button
+              onClick={() => {
+                dispatch({
+                  type: 'ADD_TO_CART',
+                  payload: prod,
+                });
+              }}
+              disabled={!prod.inStock}
+            >
+              {!prod.inStock ? 'Out of stock' : 'Add to cart'}
+            </Button>
+          )}
         </Card.Body>
       </Card>
     </div>
