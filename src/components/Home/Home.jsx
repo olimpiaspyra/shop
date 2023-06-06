88a205367e187afa0b4e8b55f2bdb96693c18d59
Olimpia Spyra <olimpia.spyra@gmail.com>
2023-06-06 19:42:47 +0200

    Add filters products to home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -9,14 +9,46 @@ import './Home.scss';
 const Home = () => {
   const {
     state: {products},
+    productState: {sort, byStock, byFastDelivery, byRating, searchQuery},
   } = CartState();
 
   console.log(products);
+
+  const transformProducts = () => {
+    let sortedProducts = products;
+
+    if (sort) {
+      sortedProducts = sortedProducts.sort((a, b) =>
+        sort === 'lowToHigh' ? a.price - b.price : b.price - a.price
+      );
+    }
+
+    if (!byStock) {
+      sortedProducts = sortedProducts.filter(prod => prod.inStock);
+    }
+
+    if (byFastDelivery) {
+      sortedProducts = sortedProducts.filter(prod => prod.fastDelivery);
+    }
+
+    if (byRating) {
+      sortedProducts = sortedProducts.filter(prod => prod.ratings >= byRating);
+    }
+
+    if (searchQuery) {
+      sortedProducts = sortedProducts.filter(prod =>
+        prod.name.toLoweCase().includes(searchQuery)
+      );
+    }
+
+    return sortedProducts;
+  };
+
   return (
     <div className='home'>
       <Filters />
       <div className='productContainer'>
-        {products.map(prod => (
+        {transformProducts().map(prod => (
           <SingleProduct key={prod.id} prod={prod} />
         ))}
       </div>
